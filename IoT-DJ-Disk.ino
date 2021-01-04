ad94602939d4a989f64400a91a15baa37be0700d
nian <yt8956789@gmail.com>
2021-01-04 23:17:29 +0800

    test.analogRead complete.

--- IoT-DJ-Disk.ino
+++ IoT-DJ-Disk.ino
@@ -12,53 +12,66 @@ int currentMusic = 1;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  KeyboardCalibrator(pin_keybroad1);
-//  softwareSerial.begin(9600);
-//  while(!player.begin(softwareSerial)){}
+  Serial.println("Keybroad start.");
+  KeyboardCalibrator1(pin_keybroad1);
+  KeyboardCalibrator2(pin_keybroad2);
+
+  for (int i = 0; i < 16; i++)
+  {
+    Serial.print(String(keybroadBtn1[i]) + ",");
+  }
+
+  for (int i = 0; i < 16; i++)
+  {
+    Serial.print(String(keybroadBtn2[i]) + ",");
+  }
+
+  //  softwareSerial.begin(9600);
+  //  while(!player.begin(softwareSerial)){}
   Serial.println("Player begin.");
-  
-//  player.volume(readVolume());
-//  player.loop(currentMusic);
+
+  //  player.volume(readVolume());
+  //  player.loop(currentMusic);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println("slide 1:    " + String(analogRead(pin_slide1)));
-  Serial.println("slide 2:    " + String(analogRead(pin_slide2)));
+  Serial.println("slider 1:   " + String(analogRead(pin_slider1)));
+  Serial.println("slider 2:   " + String(analogRead(pin_slider2)));
   Serial.println("keybroad 1: " + String(analogRead(pin_keybroad1)));
   Serial.println("keybroad 2: " + String(analogRead(pin_keybroad2)));
-  
-  sleep(1000);
-//  readkeybroadBtn(pin_keybroad1);
-//  
-//  changeMusic();
+
+  delay(1000);
+  //  readkeybroadBtn(pin_keybroad1);
+  //
+  //  changeMusic();
 }
 
-int readVolume(){
+int readVolume() {
   int value = map(analogRead(pin_slider1), 0, 1023, 0, 30);
-  if (value < 0){
+  if (value < 0) {
     value = 0;
-  } else if (value > 30){
+  } else if (value > 30) {
     value = 30;
   }
 
   return value;
 }
 
-int readkeybroadBtn(int pin){
+int readkeybroadBtn(int pin) {
   int value = analogRead(pin);
-  for (int i=1; i<=16; i++){
-    if (value <= keybroadBtn[i]*1.01 && value >= keybroadBtn[i]*0.99){
+  for (int i = 1; i <= 16; i++) {
+    if (value <= keybroadBtn1[i] * 1.01 && value >= keybroadBtn1[i] * 0.99) {
       return i;
     }
   }
   return 0;
 }
 
-void changeMusic(){
+void changeMusic() {
   int selectedMusic = readkeybroadBtn(pin_keybroad1);
-  if (currentMusic != selectedMusic && selectedMusic != 0){
-    player.play(selectedMusic);
+  if (currentMusic != selectedMusic && selectedMusic != 0) {
+//    player.play(selectedMusic);
     currentMusic = selectedMusic;
   }
 }
--- keyBroad.h
+++ keyBroad.h
@@ -1,24 +1,31 @@
-int keybroadBtn[16] = {
-    917,894,871,857,
-    841,820,801,791,
-    778,763,746,735,
-    725,709,695,687
+int keybroadBtn1[16] = {
+    0,0,0,0,
+    0,0,0,0,
+    0,0,0,0,
+    0,0,0,0
+};
+
+int keybroadBtn2[16] = {
+    0,0,0,0,
+    0,0,0,0,
+    0,0,0,0,
+    0,0,0,0
 };
 
 float btnVoltageThreshold = 0.01;
 
 int btnCount = 0;
 
-void setBtnValue(int btnValue)
+void setBtnValue1(int btnValue)
 {
   for(int i=0; i<16; i++)
   {
     if(btnValue==0) break;
-    else if(btnValue<=keybroadBtn[i]*(1.0+btnVoltageThreshold) && btnValue>=keybroadBtn[i]*(1.0-btnVoltageThreshold)){
+    else if(btnValue<=keybroadBtn1[i]*(1.0+btnVoltageThreshold) && btnValue>=keybroadBtn1[i]*(1.0-btnVoltageThreshold)){
       Serial.println("You Push Same Button Again !!");
       break;
-    } else if (keybroadBtn[i]==0){
-      keybroadBtn[i] = btnValue;
+    } else if (keybroadBtn1[i]==0){
+      keybroadBtn1[i] = btnValue;
       btnCount++;
       Serial.println("Insert New Button Value into keybroadBtn Array !!");
       break;
@@ -26,13 +33,45 @@ void setBtnValue(int btnValue)
   }
 }
 
-void KeyboardCalibrator(int pin){
+void setBtnValue2(int btnValue)
+{
+  for(int i=0; i<16; i++)
+  {
+    if(btnValue==0) break;
+    else if(btnValue<=keybroadBtn2[i]*(1.0+btnVoltageThreshold) && btnValue>=keybroadBtn2[i]*(1.0-btnVoltageThreshold)){
+      Serial.println("You Push Same Button Again !!");
+      break;
+    } else if (keybroadBtn2[i]==0){
+      keybroadBtn2[i] = btnValue;
+      btnCount++;
+      Serial.println("Insert New Button Value into keybroadBtn Array !!");
+      break;
+    }
+  }
+}
+
+void KeyboardCalibrator1(int pin){
+
+  btnCount = 0;
 
   while(1)
   {
     if(btnCount == 16) return;
     int btnValue = analogRead(pin);
-    setBtnValue(btnValue);
+    setBtnValue1(btnValue);
+    delay(500);
+  }
+}
+
+void KeyboardCalibrator2(int pin){
+
+  btnCount = 0;
+
+  while(1)
+  {
+    if(btnCount == 16) return;
+    int btnValue = analogRead(pin);
+    setBtnValue2(btnValue);
     delay(500);
   }
 }
